59ab5088f8b24efe1f1b1cbab182afde5a6eb9a2
yuhanghate <714610354@qq.com>
2019-01-31 12:07:43 +0800

    [Create Project]打开mysql

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -14,50 +14,50 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-//    @Resource
-//    private UserMapper userMapper;
+    @Resource
+    private UserMapper userMapper;
 
     @Override
     public UserEntity getUserEntityOne(Long id) {
-//        return userMapper.getOne(id);
-        return null;
+        return userMapper.getOne(id);
+//        return null;
     }
 
     @Override
     public void insert(UserEntity userEntity) {
 
-//        long insert = userMapper.insert(userEntity);
-//        logger.info("插入数据 insert = "+ insert);
+        long insert = userMapper.insert(userEntity);
+        logger.info("插入数据 insert = "+ insert);
 
     }
 
     @Override
     public void update(UserEntity userEntity) {
 
-//        userMapper.update(userEntity);
+        userMapper.update(userEntity);
     }
 
     @Override
     public void delete(Long id) {
 
-//        userMapper.delete(id);
+        userMapper.delete(id);
     }
 
     @Override
     public List<UserEntity> getAll() {
-//        return userMapper.getAll();
-        return null;
+        return userMapper.getAll();
+//        return null;
     }
 
     @Override
     public List<UserEntity> getUserEntityList() {
-//        return userMapper.getUserEntityList();
-        return null;
+        return userMapper.getUserEntityList();
+//        return null;
     }
 
     @Override
     public UserEntity getUserEntitiyOne(Long id) {
-//        return userMapper.getUserEntitiyOne(id);
-        return null;
+        return userMapper.getUserEntitiyOne(id);
+//        return null;
     }
 }
